325baf0d2f6a44c2a75afac42f639a39c57a5006
Thomas Lewis <thomasalewis1028@gmail.com>
2018-10-19 01:11:01 -0500

    Hw5_P3 - Fixed Splay stuff

--- hw5_p3/src/SplayEdit.java
+++ hw5_p3/src/SplayEdit.java
@@ -1,16 +1,40 @@
-/* The Splay Tree is a variation of the Binary Search Tree
- * IE the leftCh child is smaller and the rightCh child is greater
- * leftCh most item would necessarily be the smallest and the rightCh
- * most will be the biggest item.
- */
-
 public class SplayEdit {
-
-	// BST helper node data type
-	private SplayNode head;   // head of the BST
+	private SplayNode head;
 
 	public SplayEdit() {
 		head = null;
+	}
+
+	private void Splay(SplayNode x) {
+		while (x.getParent() != null) {
+			SplayNode Parent = x.getParent();
+			SplayNode GrandParent = Parent.getParent();
+			if (GrandParent == null) {
+				if (x == Parent.getLeftCh())
+					makeLeftChildParent(x, Parent);
+				else
+					makeRightChildParent(x, Parent);
+			} else {
+				if (x == Parent.getLeftCh()) {
+					if (Parent == GrandParent.getLeftCh()) {
+						makeLeftChildParent(Parent, GrandParent);
+						makeLeftChildParent(x, Parent);
+					} else {
+						makeLeftChildParent(x, x.getParent());
+						makeRightChildParent(x, x.getParent());
+					}
+				} else {
+					if (Parent == GrandParent.getLeftCh()) {
+						makeRightChildParent(x, x.getParent());
+						makeLeftChildParent(x, x.getParent());
+					} else {
+						makeRightChildParent(Parent, GrandParent);
+						makeRightChildParent(x, Parent);
+					}
+				}
+			}
+		}
+		head = x;
 	}
 
 	void insert(int num) {
@@ -39,14 +63,20 @@ public class SplayEdit {
 		Splay(tempRoot);
 	}
 
-	/* Rotates the node with the parent node rotating it leftCh. */
-	private void makeLeftChildParent(SplayNode c, SplayNode p)
-	{
+	void delete(int num) {
+		SplayNode temp = isDuplicate(num);
+		deleteNum(temp);
+	}
+
+	boolean search(int key) {
+		return isDuplicate(key) != null;
+	}
+
+	private void makeLeftChildParent(SplayNode c, SplayNode p) {
 		if ((c == null) || (p == null) || (p.getLeftCh() != c) || (c.getParent() != p))
 			throw new RuntimeException("WRONG");
 
-		if (p.getParent() != null)
-		{
+		if (p.getParent() != null) {
 			if (p == p.getParent().getLeftCh())
 				p.getParent().setLeftCh(c);
 			else
@@ -61,13 +91,10 @@ public class SplayEdit {
 		c.setRightCh(p);
 	}
 
-	/* Rotates the node with the parent node rotating it rightCh. */
-	private void makeRightChildParent(SplayNode c, SplayNode p)
-	{
+	private void makeRightChildParent(SplayNode c, SplayNode p) {
 		if ((c == null) || (p == null) || (p.getRightCh() != c) || (c.getParent() != p))
 			throw new RuntimeException("WRONG");
-		if (p.getParent() != null)
-		{
+		if (p.getParent() != null) {
 			if (p == p.getParent().getLeftCh())
 				p.getParent().setLeftCh(c);
 			else
@@ -81,93 +108,27 @@ public class SplayEdit {
 		c.setLeftCh(p);
 	}
 
-	/* Splay Function that calls upon the zig, zigzag or zigzig function. */
-	private void Splay(SplayNode x)
-	{
-		while (x.getParent() != null)
-		{
-			SplayNode Parent = x.getParent();
-			SplayNode GrandParent = Parent.getParent();
-			if (GrandParent == null)
-			{
-				if (x == Parent.getLeftCh())
-					makeLeftChildParent(x, Parent);
-				else
-					makeRightChildParent(x, Parent);
-			}
-			else
-			{
-				if (x == Parent.getLeftCh())
-				{
-					if (Parent == GrandParent.getLeftCh())
-					{
-						makeLeftChildParent(Parent, GrandParent);
-						makeLeftChildParent(x, Parent);
-					}
-					else
-					{
-						makeLeftChildParent(x, x.getParent());
-						makeRightChildParent(x, x.getParent());
-					}
-				}
-				else
-				{
-					if (Parent == GrandParent.getLeftCh())
-					{
-						makeRightChildParent(x, x.getParent());
-						makeLeftChildParent(x, x.getParent());
-					}
-					else
-					{
-						makeRightChildParent(Parent, GrandParent);
-						makeRightChildParent(x, Parent);
-					}
-				}
-			}
-		}
-		head = x;
-	}
-
-	/* Function that initializes the function to delete the element
-	 * we are wanting to delete from the Splay Tree. */
-	void delete(int num)
-	{
-		SplayNode temp = isDuplicate(num);
-		deleteNum(temp);
-	}
-
-	/* Function that traverses through the Splay tree to delete the
-	 * element. */
-	private void deleteNum(SplayNode node)
-	{
-		// Checks if Splay Tree is empty.
+	private void deleteNum(SplayNode node) {
 		if (node == null)
 			return;
 
 		Splay(node);
-		if( (node.getLeftCh() != null) && (node.getRightCh() !=null))
-		{
+		if ((node.getLeftCh() != null) && (node.getRightCh() != null)) {
 			SplayNode min = node.getLeftCh();
-			while(min.getRightCh() !=null)
+			while (min.getRightCh() != null)
 				min = min.getRightCh();
 
 			min.setRightCh(node.getRightCh());
 			node.getRightCh().setParent(min);
 			node.getLeftCh().setParent(null);
 			head = node.getLeftCh();
-		}
-		else if (node.getRightCh() != null)
-		{
+		} else if (node.getRightCh() != null) {
 			node.getRightCh().setParent(null);
 			head = node.getRightCh();
-		}
-		else if( node.getLeftCh() !=null)
-		{
+		} else if (node.getLeftCh() != null) {
 			node.getLeftCh().setParent(null);
 			head = node.getLeftCh();
-		}
-		else
-		{
+		} else {
 			head = null;
 		}
 		node.setParent(null);
@@ -175,50 +136,30 @@ public class SplayEdit {
 		node.setRightCh(null);
 	}
 
-	/* Boolean Function that calls the isDuplicate function to check if our
-	 * key we are looking for is in the Tree*/
-	boolean search(int key)
-	{
-		return isDuplicate(key) != null;
-	}
-
-	/* Checks to see if the Integer being inserted into the Splay Tree is a duplicate
-	 * of an element that is already in the Splay Tree */
-	private SplayNode isDuplicate(int key)
-	{
+	private SplayNode isDuplicate(int key) {
 		SplayNode temp = head;
 		while (temp != null) {
 			if (key < temp.getKey()) {
 				temp = temp.getLeftCh();
-			}
-			else if (key > temp.getKey()) {
+			} else if (key > temp.getKey()) {
 				temp = temp.getLeftCh();
-			}
-			else {
+			} else {
 				return temp;
 			}
 		}
 		return null;
 	}
 
-	/* Function that calls the inorder Print function*/
-	void inorder()
-	{
-		inorderPrint(head);
+	void printSplay() {
+		printSplay(head);
 	}
 
-	/* Function that prints out the nodes in the Splay Tree
-	 * in - order from rightCh to leftCh. */
-	private void inorderPrint(SplayNode root)
-	{
-		if (root != null)
-		{
-			//recursive call to check the rightCh side of the tree first.
-			inorderPrint(root.getLeftCh());
+	private void printSplay(SplayNode root) {
+		if (root != null) {
+			printSplay(root.getLeftCh());
 			System.out.print(root.getKey() + " ");
 
-			// recursive call to the function to check the leftCh side of the tree.
-			inorderPrint(root.getRightCh());
+			printSplay(root.getRightCh());
 		}
 	}
 }
